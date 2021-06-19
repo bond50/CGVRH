7fff8ddd79ac78b765a78c8c0e880578e67a105f
bond50 <galavu10@gmail.com>
2021-06-19 16:14:12 +0300

    seo rendering of tags and categories

---
+++ frontend/pages/tags/[slug].js
@@ -0,0 +1,80 @@
+import Layout from "../../hoc/Layout";
+
+import Card from "../../components/blog/Card";
+import {singleTag} from "../../actions/tag";
+import Head from "next/head";
+import {APP_NAME, DOMAIN, FB_APP_ID} from "../../config";
+
+
+const Tag = ({tag, blogs, query}) => {
+    const head = () => (
+        <Head>
+            <title>
+                {tag.name} | {APP_NAME}
+            </title>
+            <meta name="description" content={`${APP_NAME} blog on ${tag.name}`}/>
+
+            <link rel="canonical" href={`${DOMAIN}/tags/${query.slug}`}/>
+            <meta property="og:title" content={`${tag.name}| ${APP_NAME}`}/>
+            <meta property="og:description" content={`${APP_NAME} blog on ${tag.name}`}/>
+            <meta property="og:type" content="webiste"/>
+            <meta property="og:url" content={`${DOMAIN}/tags/${query.slug}`}/>
+            <meta property="og:site_name" content={`${APP_NAME}`}/>
+
+            <meta property="og:image"
+                  content={`https://res.cloudinary.com/dwtcilinl/image/upload/v1622297993/Gallery/yffhwkqackates3w0hte.png`}/>
+            <meta property="og:image:secure_url"
+                  content={`https://res.cloudinary.com/dwtcilinl/image/upload/v1622297993/Gallery/yffhwkqackates3w0hte.png`}/>
+            <meta property="og:image:type" content="image/png"/>
+            <meta property="fb:app_id" content={`${FB_APP_ID}`}/>
+
+        </Head>
+    );
+
+
+    return (
+        <>
+            {head()}
+            <Layout>
+                <main>
+                    <div className="container-fluid text-center">
+                        <header>
+                            <div className="col-md-12 pt-3">
+                                <h1 className="display-4 font-weight-bold">{tag.name}</h1>
+
+                                {blogs.map((b, i) => {
+                                        return <div>
+                                            <Card key={i} blog={b}/>
+                                            <hr/>
+                                        </div>
+                                    }
+                                )}
+                            </div>
+                        </header>
+                    </div>
+                </main>
+
+            </Layout>
+        </>
+    );
+};
+export const getServerSideProps = async ({query}) => {
+
+    return singleTag(query.slug).then(data => {
+
+        if (data.error) {
+            console.log(data.error)
+        } else {
+
+            return {
+                props: {
+                    tag: data.tag,
+                    blogs: data.blogs,
+                    query
+                }
+            }
+        }
+    })
+}
+
+export default Tag;
