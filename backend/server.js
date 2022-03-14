7d827b60e060c597dec643392e55f01d48b5f5fa
bond50 <galavu10@gmail.com>
2022-03-14 15:46:40 +0300

    refactor

--- backend/server.js
+++ backend/server.js
@@ -89,6 +89,7 @@ app.use('/api', galleryTagRoutes);
 app.use('/api', documentTagRoutes);
 
 
+
 process.on('uncaughtException', function (exception) {
     console.log(exception); // to see your exception details in the console
     // if you are on production, maybe you can send the exception details to your
