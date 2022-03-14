e71d342bdece4bdf50a9954b9e5a8864f0e02cc1
bond50 <galavu10@gmail.com>
2022-03-14 13:06:29 +0300

    refactor

--- frontend/components/footer/SocialLinks.js
+++ frontend/components/footer/SocialLinks.js
@@ -4,13 +4,17 @@ import classes from "../../styles/FooterSocialLinks.module.css";
 const SocialLinks = () => (
         <div className={`${classes.SocialLinks} mt-3`}>
             <a href="https://www.facebook.com/Vihiga-County-Referral-Hospital-100375528619736" className="facebook"><i
-                className="bi bi-facebook"/></a> <a
-            href="https://www.facebook.com/Vihiga-County-Referral-Hospital-100375528619736" className="facebook"><i
-            className="bi bi-facebook"/></a> <a
-            href="https://www.facebook.com/Vihiga-County-Referral-Hospital-100375528619736" className="facebook"><i
-            className="bi bi-facebook"/></a> <a
-            href="https://www.facebook.com/Vihiga-County-Referral-Hospital-100375528619736" className="facebook"><i
-            className="bi bi-facebook"/></a>
+                className="bi bi-facebook"/>
+            </a>
+            <a href="https://www.facebook.com/Vihiga-County-Referral-Hospital-100375528619736" className="facebook"><i
+                className="bi bi-facebook"/>
+            </a>
+            <a href="https://www.facebook.com/Vihiga-County-Referral-Hospital-100375528619736" className="facebook"><i
+                className="bi bi-facebook"/>
+            </a>
+            <a href="https://www.facebook.com/Vihiga-County-Referral-Hospital-100375528619736" className="facebook"><i
+                className="bi bi-facebook"/>
+            </a>
         </div>
 
     )
