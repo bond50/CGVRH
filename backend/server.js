60d099df5e4953445dd586ad4061dbf4dc001cd7
bond50 <galavu10@gmail.com>
2022-03-14 15:44:48 +0300

    refactor

--- backend/server.js
+++ backend/server.js
@@ -54,24 +54,20 @@ app.use(bodyParser.urlencoded({limit: "200mb", extended: true}));
 
 //cors
 
-if (process.env.NODE_ENV === "production") {
-    app.use(cors('vihigahospital.go.ke'))
-}
-else {
-    app.use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', '*')
-        res.header(
-            'Access-Control-Allow-Headers',
-            'Origin,X-Requested-With,Content-Type,Accept,Authorization')
 
-        if (req.method === 'OPTIONS') {
-            res.header('Access-Control-Allow-Methods', 'PUT,PATCH,POST,DELETE,GET')
-            return res.status(200).json({})
-        }
-        next()
-    })
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin,X-Requested-With,Content-Type,Accept,Authorization')
 
-}
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT,PATCH,POST,DELETE,GET')
+        return res.status(200).json({})
+    }
+    next()
+})
+
 
 // port
 const port = process.env.PORT || 8000
