9a3de1795b3d0836af69404ca3a181f19cd76f5f
bond50 <galavu10@gmail.com>
2022-03-19 19:37:50 +0300

    SEO Optimisation  Links

--- frontend/components/home/clients.js
+++ frontend/components/home/clients.js
@@ -3,12 +3,16 @@ import Link from "next/link";
 
 const Clients = () => {
     const list = [
-        {name: 'NHIF and community based organizations (CBOs).', src: '/partners/nhif1.png',to:'http://www.nhif.or.ke/healthinsurance/'},
-        {name: 'MOH', src: '/partners/moh1.png',to:'https://www.health.go.ke/'},
-        {name: 'KEMRI/Welcome Trust', src: '/partners/kemri.png',to:'https://kemri-wellcome.org/'},
-        {name: 'KMTC', src: '/partners/kmtc1.png',to:'https://kmtc.ac.ke/'},
-        {name: 'Moi University', src: '/partners/mu.png',to:'https://www.mu.ac.ke/index.php/en/'},
-        {name: 'Maseno University', src: '/partners/maseno.png',to:'https://www.maseno.ac.ke/',},
+        {
+            name: 'NHIF and community based organizations (CBOs).',
+            src: '/partners/nhif1.png',
+            to: 'http://www.nhif.or.ke/healthinsurance/'
+        },
+        {name: 'MOH', src: '/partners/moh1.png', to: 'https://www.health.go.ke/'},
+        {name: 'KEMRI/Welcome Trust', src: '/partners/kemri.png', to: 'https://kemri-wellcome.org/'},
+        {name: 'KMTC', src: '/partners/kmtc1.png', to: 'https://kmtc.ac.ke/'},
+        {name: 'Moi University', src: '/partners/mu.png', to: 'https://www.mu.ac.ke/index.php/en/'},
+        {name: 'Maseno University', src: '/partners/maseno.png', to: 'https://www.maseno.ac.ke/',},
     ]
     return (
         <section id="clients" className="clients section-bg">
@@ -16,9 +20,9 @@ const Clients = () => {
                 <div className="row" data-aos="zoom-in">
                     {list.map((item, i) => {
                         return <div key={i}
-                            className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                                    className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
                             <Link href={item.to}>
-                                 <img src={item.src} className="img-fluid" alt={item.name}/>
+                                <img src={item.src} className="img-fluid" alt={item.name} title={item.name}/>
                             </Link>
                         </div>
                     })}
