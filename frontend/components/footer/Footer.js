03644dbfe71f85cea3a20779f641f2c9d890c3b7
bond50 <galavu10@gmail.com>
2022-03-19 21:33:16 +0300

    SEO Optimisation  Links

--- frontend/components/footer/Footer.js
+++ frontend/components/footer/Footer.js
@@ -7,7 +7,6 @@ import {API} from "../../config";
 import FooterLink from "./FooterLinks/FooterLink";
 import {Fragment} from "react";
 import ScrollTop from "../ScrollTop";
-import SocialLinks from "./SocialLinks";
 
 
 const Footer = () => {
@@ -18,7 +17,7 @@ const Footer = () => {
     const list = [
         {
             title: 'Elephant System',
-            link: 'https://auth0.platform-proxy.kenya.elephantprimarycare.com/login?state=hKFo2SBLeUFVWmVrZ0Z5WWtNOHRpcTZiVHpyeW4yZzhLc0M4cKFupWxvZ2luo3RpZNkgdFBYdnlCMlk0Z0JJR3RMU2xiN2FtNjd2X05PTHJXVU6jY2lk2SBvalBNNXNFc0VVODFuajVjemFNakVCU0RMZmx0dGlpaA&client=ojPM5sEsEU81nj5czaMjEBSDLflttiih&protocol=oauth2&prompt=login&redirect_uri=https%3A%2F%2Fkenya.elephantprimarycare.com%2Fauth0%2Fcallback&scope=openid%20profile%20email&response_type=code&response_mode=query&nonce=VmVjYm9YcGhGYzVhcXRwVjNOcDBLLVR1TDM0ZGlrNWlGVW5FRDlmeU9Ndg%3D%3D&code_challenge=TYmJ6XpMyJru7nvihDZU-zRr_2kCnGEY3DtA_fsMxJ4&code_challenge_method=S256&auth0Client=eyJuYW1lIjoiYXV0aDAtcmVhY3QiLCJ2ZXJzaW9uIjoiMS4yLjAifQ%3D%3D'
+            link: 'https://kenya.elephantprimarycare.com/'
         },
         {title: 'County website', link: 'https://vihiga.go.ke/'},
         {title: 'County Vacancies', link: 'https://vihiga.go.ke/career.html'},
@@ -59,7 +58,7 @@ const Footer = () => {
                             All Rights Reserved
                         </div>
                     </div>
-                    <SocialLinks/>
+                    {/*<SocialLinks/>*/}
                 </div>
             </footer>
             <ScrollTop/>
