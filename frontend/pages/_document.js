0b48573601cbe3e20301622ed5f9276782d7be62
bond50 <galavu10@gmail.com>
2022-03-19 17:25:20 +0300

    SEO Optimisation  Links

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
             <Html lang="en">
                 <Head>
                     <meta charSet="UTF-8"/>
-
+                    <link rel="shortcut icon" href="/favicon.ico"/>
                     <link href="https://fonts.gstatic.com" rel="preconnect"/>
                     <link
                         href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
