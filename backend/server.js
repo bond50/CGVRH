a6fafc70d5bb2dd1aec4b5dc9ce54b850f728e53
bond50 <galavu10@gmail.com>
2022-03-19 14:39:10 +0300

    refactor

--- backend/server.js
+++ backend/server.js
@@ -55,23 +55,18 @@ app.use(bodyParser.urlencoded({limit: "200mb", extended: true}));
 //cors
 
 
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*')
-//     res.header(
-//         'Access-Control-Allow-Headers',
-//         'Origin,X-Requested-With,Content-Type,Accept,Authorization')
-//
-//     if (req.method === 'OPTIONS') {
-//         res.header('Access-Control-Allow-Methods', 'PUT,PATCH,POST,DELETE,GET')
-//         return res.status(200).json({})
-//     }
-//     next()
-// })
-
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin,X-Requested-With,Content-Type,Accept,Authorization')
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT,PATCH,POST,DELETE,GET')
+        return res.status(200).json({})
+    }
+    next()
+})
 
 
 // port
