558847beaad8ed8167a16e4999fceacc2a46d769
bond50 <galavu10@gmail.com>
2022-03-21 19:08:23 +0300

    massive refactor on SEO and site maps

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -26,18 +26,15 @@ class MyDocument extends Document {
                     <meta name="theme-color" content="#ffffff"/>
                     <link href="https://fonts.gstatic.com" rel="preconnect"/>
                     <link
-                        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+                        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
                         rel="stylesheet"/>
 
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
                           rel="stylesheet"
                           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
                           crossOrigin="anonymous"/>
-
-
                     <link href="/theme/bootstrap-icons/bootstrap-icons.css" rel="stylesheet"/>
 
-                    <link href="/theme/fontawesome/css/all.min.css" rel="stylesheet"/>
                 </Head>
                 <body>
                 <Main/>
