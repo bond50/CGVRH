d23e5455d02242a5d5200cc31a73e50082132688
bond50 <galavu10@gmail.com>
2022-03-21 20:10:25 +0300

    massive refactor on SEO and site maps

--- frontend/components/home/clients.js
+++ frontend/components/home/clients.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Image from "next/image";
 
+
 const Clients = () => {
+
+
     const list = [
-        {
-            name: 'NHIF and community based organizations (CBOs).',
-            src: '/partners/nhif1.png',
-        },
-        {name: 'MOH', src: '/partners/moh1.png',},
-        {name: 'KEMRI/Welcome Trust', src: '/partners/kemri.png',},
-        {name: 'KMTC', src: '/partners/kmtc1.png',},
-        {name: 'Moi University', src: '/partners/mu.png'},
-        {name: 'Maseno University', src: '/partners/maseno.png'},
+        {name: 'NHIF and community based organizations (CBOs).', src: '/partners/nhif1.png', width: 233, height: 217},
+        {name: 'MOH', src: '/partners/moh1.png', width: 255, height: 206},
+        {name: 'KEMRI/Welcome Trust', src: '/partners/kemri.png', width: 594, height: 256},
+        {name: 'KMTC', src: '/partners/kmtc1.png', width: 255, height: 255},
+        {name: 'Moi University', src: '/partners/mu.png', width: 170, height: 170},
+        {name: 'Maseno University', src: '/partners/maseno.png', width: 152, height: 170},
     ]
     return (
         <section id="clients" className="clients section-bg">
@@ -23,8 +23,8 @@ const Clients = () => {
                             className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
                             <Image
                                 src={item.src}
-                                height={70}
-                                width={70}
+                                height={item.height}
+                                width={item.width}
                                 className="img-fluid"
                                 alt={item.name}
                                 title={item.name}/>
