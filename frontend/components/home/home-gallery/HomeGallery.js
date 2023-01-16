4cfd5eb7fec5c341dce9dd805a07faacbc388808
bobd50 <galavu10@gmail.com>
2023-01-16 23:05:07 +0300

    Massive refactor

--- frontend/components/home/home-gallery/HomeGallery.js
+++ frontend/components/home/home-gallery/HomeGallery.js
@@ -72,18 +72,18 @@ const HomeGallery = (props) => {
         superLargeDesktop: {
             // the naming can be any, depends on you.
             breakpoint: {max: 4000, min: 3000},
-            items: 4,
+            items: 6,
 
         },
         desktop: {
             breakpoint: {max: 3000, min: 1024},
-            items: 3,
+            items: 5,
 
 
         },
         tablet: {
             breakpoint: {max: 1024, min: 464},
-            items: 2,
+            items: 3,
 
         },
         mobile: {
