5a92c5adeb5ec9d739c5967a665107cfb37f252e
bobd50 <galavu10@gmail.com>
2023-02-14 21:44:07 +0300

    ngrok callback

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -317,7 +317,7 @@ exports.safTest = async (req, res) => {
         `,
     };
 
-    const {data} = await axios.post(`https://0aec-102-0-0-246.in.ngrok.io /api/callback`, {stkCallback:transactionDetails.stkCallback});
+    const {data} = await axios.post(`https://0aec-102-0-0-246.in.ngrok.io/api/callback`, {stkCallback:transactionDetails.stkCallback});
     console.log(data)
 
 
