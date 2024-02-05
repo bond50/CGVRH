feaaf3133cb66ff7a3a73c81fe40d079d125142c
bobd50 <galavu10@gmail.com>
2024-02-05 19:45:51 +0300

    Refactoring navbar to accommodate more items

--- frontend/components/home/Hero.js
+++ frontend/components/home/Hero.js
@@ -1,20 +1,17 @@
 import React from "react";
 import {CldImage} from 'next-cloudinary';
-import {APP_NAME, CLOUDINARY_NAME} from "../../config";
+import {APP_NAME} from "../../config";
 
 function Hero() {
 
 
     return (
         <section id="home-hero">
-
-
             <CldImage
                 layout='fill'
                 src="project/DJI_0153_dkmqkt"
                 sizes="100vw"
                 alt="Description of my image"
-
             />
 
             <div className="container animate__animated animate__backInUp">
