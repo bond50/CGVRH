b3e3521a0422c4098273988e740af5848f0ed091
bobd50 <galavu10@gmail.com>
2024-04-13 12:22:53 +0300

    ssr

--- frontend/components/about/Role.js
+++ frontend/components/about/Role.js
@@ -1,44 +1,48 @@
 import React from 'react';
-import {APP_NAME} from "../../config";
-import {Icon} from "@iconify/react";
+import { APP_NAME } from "../../config";
+import { Icon } from "@iconify/react";
+import Link from "next/link";
 
+const services = [
+    { name: "internal medicine", href: "/services/internal-medicine" },
+    { name: "general surgery", href: "/services/precision-and-expertise-in-surgical-care" },
+    { name: "pediatrics", href: "/services/pediatrics" },
+    { name: "obstetrics and gynecology", href: "/services/comprehensive-women's-health-services" },
+    { name: "dental services", href: "/services/dental-services" },
+    { name: "psychiatry", href: "/services/psychiatry-services" },
+    { name: "ophthalmology", href: "/services/ophthalmic-services" },
+    { name: "pharmaceutical services", href: "/services/pharmacy" },
+    { name: "ambulatory and emergency services", href: "/services/ambulance-services" },
+    { name: "laboratory services", href: "/services/laboratory" },
+    { name: "rehabilitative care", href: "/services/rehabilitative-care-services" },
+    { name: "counseling", href: "/services/counseling-services" },
+    { name: "physiotherapy", href: "/services/physiotherapy-services" },
+    { name: "nutritional services", href: "/services/nutrition-services" },
+    { name: "radiological imaging services", href: "/services/radiology-and-imaging-services" },
+    { name: "oncology services", href: "/services/oncology-services" }
+];
 
 const Role = () => {
-
-
-    return <>
-
-        <p>
-            {APP_NAME} serves as a county referral hospital, an intermediary to national and teaching referral
-            hospitals, and a center for specialized healthcare.
-        </p>
-        <p>The hospital actively engages in teaching and
-            training healthcare personnel, offering a wide range of clinical services, and contributing to
-            health research programs.</p>
-        <p>Its specialized personnel and services cover
+    return (
+        <>
+            <p>
+                {APP_NAME} serves as a county referral hospital, an intermediary to national and teaching referral
+                hospitals, and a center for specialized healthcare.
+            </p>
+            <p>The hospital actively engages in teaching and
+                training healthcare personnel, offering a wide range of clinical services, and contributing to
+                health research programs.</p>
+            <p>Its specialized personnel and services cover:</p>
             <ul>
-                <li><Icon icon="icon-park-outline:right-c" className="ico1" />internal medicine</li>
-                <li><Icon icon="icon-park-outline:right-c" className="ico1" />general surgery</li>
-                <li><Icon icon="icon-park-outline:right-c" className="ico1" /> pediatrics</li>
-                <li> <Icon icon="icon-park-outline:right-c" className="ico1" />obstetrics and gynecology</li>
-                <li><Icon icon="icon-park-outline:right-c" className="ico1" />dental services</li>
-                <li><Icon icon="icon-park-outline:right-c" className="ico1" />psychiatry</li>
-                <li><Icon icon="icon-park-outline:right-c" className="ico1" />ophthalmology</li>
-                <li><Icon icon="icon-park-outline:right-c" className="ico1" />pharmaceutical services</li>
-                <li><Icon icon="icon-park-outline:right-c" className="ico1" />ambulatory and emergency services</li>
-                <li><Icon icon="icon-park-outline:right-c" className="ico1" />laboratory services</li>
-                <li><Icon icon="icon-park-outline:right-c" className="ico1" />rehabilitative care</li>
-                <li><Icon icon="icon-park-outline:right-c" className="ico1" />counseling</li>
-                <li><Icon icon="icon-park-outline:right-c" className="ico1" />physiotherapy</li>
-                <li><Icon icon="icon-park-outline:right-c" className="ico1" />nutritional services</li>
-                <li><Icon icon="icon-park-outline:right-c" className="ico1" />radiological imaging services</li>
-                <li><Icon icon="icon-park-outline:right-c" className="ico1" />oncology services</li>
+                {services.map((service, index) => (
+                    <li key={index}>
+                        <Icon icon="icon-park-outline:right-c" className="ico1"/>
+                        <Link href={service.href}>{service.name}</Link>
+                    </li>
+                ))}
             </ul>
-        </p>
-
-
-    </>
-
+        </>
+    );
 };
 
 export default Role;
