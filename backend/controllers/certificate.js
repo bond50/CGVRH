8d7e6aab2faab4c778a47e1cccff3ff8ae06736f
bobd50 <galavu10@gmail.com>
2024-05-04 15:21:04 +0300

    ssr

--- backend/controllers/certificate.js
+++ backend/controllers/certificate.js
@@ -189,41 +189,51 @@ exports.downloadPDF = async (req, res) => {
 
         const margin = 70; // Set a margin for general alignment
         const maxWidth = 140;
-        const maxHeight = 50;
-
         const statementWidth = 600; // Set a specific width for the statement
         let statementX = doc.page.width / 2 - statementWidth / 2; // Calculate X to center the statement
-
-
-        // Add the background image
-        doc.image('assets/cert1.png', 0, 0, {width: doc.page.width, height: doc.page.height});
-
-        // Header: County Government, Department of Health
-        doc.font('fonts/Cardo-Bold.ttf').fontSize(18).fill('#021c27')
-            .text("COUNTY GOVERNMENT OF VIHIGA", margin, margin, {width: doc.page.width - 2 * margin, align: 'center'});
-
-        doc.font('fonts/Cardo-Bold.ttf').fontSize(13).fill('#021c27')
-            .text("DEPARTMENT OF HEALTH", margin, margin + 25, {width: doc.page.width - 2 * margin, align: 'center'});
-
         // Logo dimensions and scaling
         const originalLogoWidth = 298;
         const originalLogoHeight = 169;
-        const scale = 0.3;  // Scaling factor, adjust as needed for balance between visibility and space
+        const scale = 0.33;  // Scaling factor, adjust as needed for balance between visibility and space
         const logoWidth = originalLogoWidth * scale;
         const logoHeight = originalLogoHeight * scale;
         const logoX = doc.page.width / 2 - logoWidth / 2;  // Center the scaled logo
-        const logoY = margin + 50;
-
-        // Logo and contact details layout
-
-        // Place the logo with scaled dimensions
-        doc.image('assets/logo.png', logoX, logoY, {width: logoWidth, height: logoHeight});
-
-
+        const logoY = margin + 70;
         // Define fixed start positions for the text blocks relative to the page center, maintaining uniform margins
-        const textBlockWidth = 180; // Adjust the width of the text blocks if needed
+        const textBlockWidth = maxWidth; // Adjust the width of the text blocks if needed
         const leftTextX = logoX - textBlockWidth - 50; // Position left text block to the left of the logo with some spacing
         const rightTextX = logoX + logoWidth + 50; // Position right text block to the right of the logo with some spacing
+        const boxWidth = doc.page.width / 2;  // Set box width for the details table
+        let boxY = margin + 170; // Starting Y position for the details
+        const labelWidth = 160;
+        const valueWidth = boxWidth - labelWidth;// Calculate the value width as the remaining space within the boxWidth
+        const columnWidth = doc.page.width / 3; // Divide page into three columns
+        const qrCodeSize = 80; // Size of the QR code
+        const qrCodePosition = (doc.page.width / 2) - (qrCodeSize / 2); // Center position for QR code
+
+        const leftColumnStart = doc.page.width / 2 - maxWidth - 60// Start of left column with additional margin
+        const rightColumnStart = 2 * columnWidth - 25; // Start of right column
+        const signatureSpacing = 48; // Space between signature blocks
+        const isDev = process.env.NODE_ENV === 'development';
+        const baseUrl = isDev ? process.env.CLIENT_URL_DEV : process.env.CLIENT_URL;
+        const link = `${baseUrl}/certificate/verify/${id}`;
+
+
+        // Add the background image
+        doc.image('assets/cert4.png', 0, 0, {width: doc.page.width, height: doc.page.height});
+
+        // Header: County Government, Department of Health
+        doc.font('fonts/Cardo-Bold.ttf').fontSize(18).fill('#021c27')
+            .text("COUNTY GOVERNMENT OF VIHIGA", margin, margin + 20, {
+                width: doc.page.width - 2 * margin,
+                align: 'center'
+            });
+
+        doc.font('fonts/Cardo-Bold.ttf').fontSize(13).fill('#021c27')
+            .text("DEPARTMENT OF HEALTH", margin, margin + 45, {width: doc.page.width - 2 * margin, align: 'center'});
+
+
+        doc.image('assets/logo.png', logoX, logoY, {width: logoWidth, height: logoHeight});
 
 
         // Left-aligned text block
@@ -235,14 +245,14 @@ exports.downloadPDF = async (req, res) => {
 
         // Right-aligned text block
         doc.font('fonts/Cardo-Italic.ttf').fontSize(7).fill('#021c27')
-            .text(`OFFICE OF THE MEDICAL SUPERINTENDENT\nVIHIGA COUNTY REFERRAL HOSPITAL\nP.O. BOX 1069 – 50300 MARAGOLI.\n${createdAt}`, rightTextX, logoY, {
+            .text(`Office of the Medical Superintendent\n${facilityName}\nP.O. Box 1069 – 50300 Maragoli.\n${createdAt}`, rightTextX, logoY, {
                 width: textBlockWidth,
                 align: 'left'
             });
 
         // Place the 'CERTIFICATE' text
-        doc.font('fonts/CinzelDecorative-Bold.ttf').fontSize(18).fill('#021c27')
-            .text('CERTIFICATE OF COMPLETION', margin, margin + 120, {
+        doc.font('fonts/CinzelDecorative-Bold.ttf').fontSize(14).fill('#021c27')
+            .text('CERTIFICATE OF COMPLETION', margin, margin + 140, {
                 width: doc.page.width - 2 * margin,
                 align: 'center'
             });
@@ -266,94 +276,91 @@ exports.downloadPDF = async (req, res) => {
             ["Certificate Amount", `KES ${formattedAmount}`],
         ];
 
-        const boxWidth = doc.page.width / 2;  // Set box width for the details table
-        let boxY = margin + 160; // Starting Y position for the details
-
-        const labelWidth = 150;
-        const valueWidth = boxWidth - labelWidth;// Calculate the value width as the remaining space within the boxWidth
-
-
-        const tableWidth = labelWidth + valueWidth; // Calculate the total width of the table
-        const tableStartX = (doc.page.width - tableWidth) / 2; // Calculate the starting X position to center the table
 
         details.forEach(([label, value]) => {
             // Draw the label left-aligned within its designated space
-            doc.font('fonts/Cardo-Regular.ttf').fontSize(8).fill('#021c27')
-                .text(`${label}:`, tableStartX, boxY, {width: labelWidth, align: 'left'});
-
+            doc.font('fonts/Fraunces_72pt-Regular.ttf').fontSize(8).fill('#021c27')
+                .text(`${label}:`, leftColumnStart, boxY, {width: labelWidth, align: 'left'});
             // Draw the value left-aligned within its designated space
-            doc.font('fonts/Cardo-Bold.ttf').fontSize(8).fill('#021c27')
-                .text(value, tableStartX + labelWidth, boxY, {width: valueWidth, align: 'left'});
-            boxY += 15; // Increment Y position for the next row
+            doc.font('fonts/Fraunces_72pt-Regular.ttf').fontSize(8).fill('#021c27')
+                .text(value, leftColumnStart + labelWidth, boxY, {width: valueWidth, align: 'left'});
+            boxY += 12; // Increment Y position for the next row
         });
 
-        boxY += 5; // Gap before the statement
+        boxY += 15; // Gap before the statement
         const cleanDefectsText = defectsText.replace(/\n/g, ' ');
 
-        doc.font('fonts/Poppins-Light.ttf').fontSize(9).fill('#021c27')
+        doc.font('fonts/Cardo-Regular.ttf').fontSize(9).fill('#021c27')
             .text(cleanDefectsText, statementX, boxY, {
                 width: statementWidth,
                 align: 'center'
             });
 
-        boxY += 20; // Ensure enough space after the statement
+        boxY += 30; // Ensure enough space after the statement
 
 
         const signatures = [
-            {title: "Project Manager", name: projectManager.toUpperCase()},
-            {title: "Facility In-charge", name: facilityIncharge.toUpperCase()}, // Adjust name as needed
-            {title: "Contractor", name: contractor.toUpperCase()},
-            {title: "Supervisor", name: projectSupervisor.toUpperCase()} // Adjust name as needed
+            {title: "Project Manager", name: projectManager},
+            {title: "Facility In-charge", name: facilityIncharge},
+            {title: "Contractor", name: contractor},
+            {title: "Project Supervisor", name: projectSupervisor},
         ];
 
-        // Initialize variables for the positions and dimensions
-        const columnWidth = doc.page.width / 3; // Divide page into three columns
-        const qrCodeSize = 80; // Size of the QR code
-        const qrCodePosition = (doc.page.width / 2) - (qrCodeSize / 2); // Center position for QR code
-        const leftColumnStart = doc.page.width / 2 - maxWidth - 60// Start of left column with additional margin
-        const rightColumnStart = 2 * columnWidth - 25; // Start of right column
-        const signatureSpacing = 40; // Space between signature blocks
 
-// Generate QR Code and place in the center
-
-        const isDev = process.env.NODE_ENV === 'development';
-        const baseUrl = isDev ? process.env.CLIENT_URL_DEV : process.env.CLIENT_URL;
-        const link = `${baseUrl}/certificate/verify/${id}`;
+        // Generate QR Code and place in the center
 
 
         const qrCodeURL = await QRCode.toDataURL(link);
         doc.image(qrCodeURL, qrCodePosition, boxY, {fit: [qrCodeSize, qrCodeSize]});
 
-// Place signatures on the left
+        doc.font('fonts/NotoSerif_Condensed-Bold.ttf').fontSize(6).fill('#021c27')
+            .text(`SN: ${serialNumber}`, qrCodePosition + 14, boxY + qrCodeSize + 1);
+
+        const labelX = leftColumnStart;
+        const valueX = leftColumnStart + doc.widthOfString('Name: ')  // Start the name right after the label
+
+        // Place signatures on the left
         signatures.slice(0, 2).forEach(({title, name}, idx) => {
             const yPosition = boxY + (idx * signatureSpacing); // Position each signature block with defined spacing
 
             // Print title, name, and signature in order
-            doc.font('fonts/AlexBrush-Regular.ttf').fontSize(14).fill('#021c27')
+            doc.font('fonts/NotoSerif_Condensed-Bold.ttf').fontSize(8).fill('#021c27')
                 .text(title, leftColumnStart, yPosition);
-            doc.font('fonts/Poppins-Regular.ttf').fontSize(6).fill('#021c27')
-                .text(`Name: ${name}`, leftColumnStart, yPosition + 15);
-            doc.font('fonts/Poppins-Regular.ttf').fontSize(6).fill('#021c27')
-                .text(`Sign: `, leftColumnStart, yPosition + 30);
+            // Print the "Name" label
+            doc.font('fonts/NotoSerif_Condensed-Regular.ttf').fontSize(8).fill('#021c27')
+                .text('Name: ', labelX, yPosition + 15);
+            // Print the name value
+            doc.font('fonts/Italianno-Regular.ttf').fontSize(13).fill('#021c27')
+                .text(name, valueX +  2, yPosition + 13);
+            doc.font('fonts/NotoSerif_Condensed-Regular.ttf').fontSize(8).fill('#021c27')
+                .text(`Sign: `, leftColumnStart, yPosition + 28);
         });
 
 // Place signatures on the right
         signatures.slice(2, 4).forEach(({title, name}, idx) => {
             const yPosition = boxY + (idx * signatureSpacing); // Position each signature block with defined spacing
 
-            // Print title, name, and signature in order
-            doc.font('fonts/AlexBrush-Regular.ttf').fontSize(14).fill('#021c27')
+            // Print title in order
+            doc.font('fonts/NotoSerif_Condensed-Bold.ttf').fontSize(8).fill('#021c27')
                 .text(title, rightColumnStart, yPosition);
-            doc.font('fonts/Poppins-Regular.ttf').fontSize(6).fill('#021c27')
-                .text(`Name: ${name}`, rightColumnStart, yPosition + 15);
-            doc.font('fonts/Poppins-Regular.ttf').fontSize(6).fill('#021c27')
-                .text(`Sign: `, rightColumnStart, yPosition + 30);
+
+            // Print the "Name" label
+            doc.font('fonts/NotoSerif_Condensed-Regular.ttf').fontSize(8).fill('#021c27')
+                .text('Name: ', rightColumnStart, yPosition + 15);
+
+            // Calculate the X position for the name value to align properly
+            const labelX = rightColumnStart;
+            const valueX = rightColumnStart + doc.widthOfString('Name: '); // Start the name right after the label
+
+            // Print the name value using a different font
+            doc.font('fonts/Italianno-Regular.ttf').fontSize(13).fill('#021c27')
+                .text(name, valueX, yPosition + 13);
+
+            // Print signature label
+            doc.font('fonts/NotoSerif_Condensed-Regular.ttf').fontSize(8).fill('#021c27')
+                .text(`Sign: `, rightColumnStart, yPosition + 28);
         });
 
-        const serialNumberX = margin;
-        const serialNumberY = doc.page.height - 130; // Adjust Y position to align with the bottom
-        doc.font('fonts/Poppins-Regular.ttf').fontSize(9).fill('#021c27')
-            .text(`SN: ${serialNumber}`, serialNumberX, serialNumberY);
 
         doc.end();
 
