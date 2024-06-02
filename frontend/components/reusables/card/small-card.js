17524932a106bc5ba92f8c7f60716144cf5cb515
bobd50 <galavu10@gmail.com>
2024-06-02 17:11:56 +0300

    improving blog page  seo and image

--- frontend/components/reusables/card/small-card.js
+++ frontend/components/reusables/card/small-card.js
@@ -1,55 +1,62 @@
 import Link from 'next/link';
 import renderHTML from 'react-render-html';
 import moment from 'moment';
-import {API} from '../../../config';
-import classes from '../../../styles/SmallCard.module.css'
+import { API } from '../../../config';
+import classes from '../../../styles/SmallCard.module.css';
 import Image from "next/image";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const SmallCard = ({blog, service}) => {
+const SmallCard = ({ blog, service }) => {
+    const [photoLink, setPhotoLink] = useState('');
+    const [multiLink, setMultiLink] = useState('');
 
-    console.log(blog)
+    useEffect(() => {
+        let initialPhotoLink;
+        let initialMultiLink;
 
-
-    let photoLink
-    let multiLink
-
-    if (service) {
-        photoLink = `${API}/service/photo/${service.slug}`
-        multiLink = `/services/${service.slug}`
-    } else if (blog) {
-        multiLink = `/blogs/${blog.slug}`
-        if (blog.images && blog.images.length && blog.images.length > 0) {
-            const image = blog.images[Math.floor(Math.random() * blog.images.length)];
-            photoLink = image.url
+        if (service) {
+            initialPhotoLink = `${API}/service/photo/${service.slug}`;
+            initialMultiLink = `/services/${service.slug}`;
+        } else if (blog) {
+            initialMultiLink = `/blogs/${blog.slug}`;
+            if (blog.images && blog.images.length > 0) {
+                const image = blog.images[0]; // Ensure consistent image selection
+                initialPhotoLink = image.url;
+            } else {
+                initialPhotoLink = `${API}/blog/photo/${blog.slug}`;
+            }
         } else {
-            photoLink = `${API}/blog/photo/${blog.slug}`
+            initialPhotoLink = '';
+            initialMultiLink = '';
         }
 
+        const cloudinaryUrlTransform = (url) => {
+            return url.replace('/upload/', '/upload/f_auto,q_auto/');
+        };
 
-    } else {
-        photoLink = ''
-        multiLink = ''
+        if (initialPhotoLink) {
+            initialPhotoLink = cloudinaryUrlTransform(initialPhotoLink);
+        }
+
+        setPhotoLink(initialPhotoLink);
+        setMultiLink(initialMultiLink);
+    }, [blog, service]);
+
+    if (!photoLink) {
+        return null; // Don't render the component until the photoLink is set
     }
 
-
-    const returnCard = () => {
-        const myLoader = () => {
-            return photoLink;
-        }
-
-        return <div className={`card ${classes.Card}`}>
+    return (
+        <div className={`card ${classes.Card}`}>
             <Link href={multiLink}>
                 <a>
                     <Image
-                        loader={myLoader}
-                        style={{maxHeight: 'auto', width: '100%'}}
-                        className="img-fluid"
+                        layout="responsive"
                         width={720}
                         height={450}
                         src={photoLink}
-                        priority={true}
                         alt={blog ? blog.title : service.title}
+                        priority={true} // Preload the first image
                     />
                 </a>
             </Link>
@@ -61,20 +68,20 @@ const SmallCard = ({blog, service}) => {
                         </a>
                     </Link>
                 </h5>
-
-                <div className="card-text">{blog ? renderHTML(blog.excerpt) : renderHTML(service.excerpt)}</div>
-                {blog && <div className={classes.Info}>
-                    Posted {moment(blog.createdAt).fromNow()} by{' '}
-                    <Link href={`/profile/${blog.postedBy.username}`}>
-                        <a className="float-end">{blog.postedBy.username}</a>
-                    </Link>
+                <div className="card-text">
+                    {blog ? renderHTML(blog.excerpt) : renderHTML(service.excerpt)}
                 </div>
-                }
+                {blog && (
+                    <div className={classes.Info}>
+                        Posted {moment(blog.createdAt).fromNow()} by{' '}
+                        <Link href={`/profile/${blog.postedBy.username}`}>
+                            <a className="float-end">{blog.postedBy.username}</a>
+                        </Link>
+                    </div>
+                )}
             </div>
-        </div>;
-    };
-
-    return (returnCard());
+        </div>
+    );
 };
 
 export default SmallCard;
