ca6101d109cb83f6d1e36d23b9049cd29cce0e44
bobd50 <galavu10@gmail.com>
2024-06-02 12:48:11 +0300

    google insights

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -12,9 +12,10 @@ class MyDocument extends Document {
             <Html lang="en">
                 <Head>
                     <meta charSet="UTF-8"/>
-                    {ADSENSE_CLIENT_ID && (
-                        <meta name="google-adsense-account" content={ADSENSE_CLIENT_ID}/>
+                     {ADSENSE_CLIENT_ID && (
+                        <meta name="google-adsense-account" content={ADSENSE_CLIENT_ID} />
                     )}
+
                     <meta name="google-adsense-account" content="ca-pub-9278372397525835"/>
                     <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
                     <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
@@ -33,37 +34,26 @@ class MyDocument extends Document {
                     <meta name="msapplication-TileColor" content="#ffffff"/>
                     <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
                     <meta name="theme-color" content="#ffffff"/>
-
                     <link href="https://fonts.gstatic.com" rel="preconnect"/>
+
+
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true'/>
-
                     <link
-                        rel="preload"
-                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-                        as="style"
-                        onLoad="this.onload=null;this.rel='stylesheet'"
-                    />
-                    <noscript>
-                        <link
-                            rel="stylesheet"
-                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-                        />
-                    </noscript>
+                        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Roboto:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Questrial&display=swap"
+                        rel="stylesheet"/>
 
-                    <link
-                        rel="preload"
-                        href="/theme/bootstrap-icons/bootstrap-icons.css"
-                        as="style"
-                        onLoad="this.onload=null;this.rel='stylesheet'"
-                    />
-                    <noscript>
-                        <link rel="stylesheet" href="/theme/bootstrap-icons/bootstrap-icons.css" />
-                    </noscript>
+
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+                          rel="stylesheet"
+                          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+                          crossOrigin="anonymous"/>
+                    <link href="/theme/bootstrap-icons/bootstrap-icons.css" rel="stylesheet"/>
+
                 </Head>
                 <body>
-                    <Main/>
-                    <NextScript/>
+                <Main/>
+                <NextScript/>
                 </body>
             </Html>
         );
