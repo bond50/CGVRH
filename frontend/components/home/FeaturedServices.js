f4227ded52a74c259583316f6342eb28d65b0f4c
bobd50 <galavu10@gmail.com>
2024-07-31 19:01:05 +0300

    Massive refactor

--- frontend/components/home/FeaturedServices.js
+++ frontend/components/home/FeaturedServices.js
@@ -1,62 +1,55 @@
 import Link from "next/link";
-import {API, APP_NAME} from "../../config";
+import { API, APP_NAME } from "../../config";
 import ServiceCard from "../reusables/card/serviceCard/ServiceCard";
 
 import React from "react";
-import {generateExcerpt} from "../reusables/functions/generate-excerpt";
-import styles from '../../styles/FeaturedServices.module.css'
+import { generateExcerpt } from "../reusables/functions/generate-excerpt";
+import styles from '../../styles/FeaturedServices.module.css';
 
-
-const FeaturedServices = ({services}) => {
-
+const FeaturedServices = ({ services }) => {
+    const defaultImage = '/home/image.jpg';
 
     return (
-
         <section className={styles.Featured}>
             <div className="container" data-aos="slide-up">
                 <div className="section-title">
                     <h2>
                         <Link href={`/services`}>
-                          Services
+                            Services
                         </Link>
                     </h2>
                     <h3>Explore our <span>featured <Link href={`/services`}>services</Link></span></h3>
-
                     <p>Explore the distinctive attributes that distinguish the services offered at {APP_NAME}.</p>
-
-
                 </div>
 
                 <div className="row gy-3">
                     {services && services.map((service, i) => {
-
-                        let imgSrc = `${API}/general/photo/${service.slug}`
-                        if (service.images && service.images.length && service.images.length > 0) {
+                        let imgSrc = `${API}/general/photo/${service.slug}`;
+                        if (service.images && service.images.length > 0) {
                             const image = service.images[Math.floor(Math.random() * service.images.length)];
-                            imgSrc = image.url
+                            imgSrc = image.url;
+                        } else {
+                            imgSrc = defaultImage;
                         }
 
-
-                        return <ServiceCard
-                            title={service.title}
-                            delay={`${i * 100}`}
-                            href={`/services/${service.slug}`}
-                            imgSrc={imgSrc}
-                            imgAlt={service.title}
-                            key={service._id}
-                        >
-                            <p>{generateExcerpt(service.excerpt,100)}</p>
-                        </ServiceCard>
-
+                        return (
+                            <ServiceCard
+                                title={service.title}
+                                delay={`${i * 100}`}
+                                href={`/services/${service.slug}`}
+                                imgSrc={imgSrc}
+                                imgAlt={service.title}
+                                key={service._id}
+                            >
+                                <p>{generateExcerpt(service.excerpt, 100)}</p>
+                            </ServiceCard>
+                        );
                     })}
                 </div>
             </div>
 
             {/*<WaveBottom/>*/}
-
         </section>
-
-
     );
 };
 
