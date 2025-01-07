75825ec64094bf094d73546808f1e4a815d53367
bobd50 <galavu10@gmail.com>
2025-01-07 14:30:30 +0300

    refactor

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -29,11 +29,15 @@ class MyDocument extends Document {
                     <meta name="msapplication-TileColor" content="#ffffff"/>
                     <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
                     <meta name="theme-color" content="#ffffff"/>
-                    <link href="https://fonts.gstatic.com" rel="preconnect"/>
-                    <link href="https://fonts.googleapis.com" rel="preconnect"/>
-                    <link href="https://fonts.gstatic.com" rel="preconnect" crossOrigin='true'/>
-                    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
-                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"/>
+                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+                        rel="stylesheet"/>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+                          rel="stylesheet"
+                          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+                          crossOrigin="anonymous"/>
 
                 </Head>
                 <body>
